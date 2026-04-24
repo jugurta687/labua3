3a334c7168f52a39ce9e1198c30fe2bf4d56c393
wafa2747 <2747233@collegelacite.ca>
2026-04-23 23:43:50 -0400

    Update RapportWriter.java
    Ajout de commentaires

--- Wafa/RapportWriter.java
+++ Wafa/RapportWriter.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//Interface définissant le contrat pour la génération de rapports
 public interface RapportWriter {
     void genererRapport(List<Statistique> statistiques, String nomFichier);
 }
