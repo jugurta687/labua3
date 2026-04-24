c58bf27f014b71557612fc1f55a3a8504978db08
wafa2747 <2747233@collegelacite.ca>
2026-04-23 23:46:10 -0400

    Update AnalyseurAbstrait.java
    Ajout de commentaires

--- Jugurta/AnalyseurAbstrait.java
+++ Jugurta/AnalyseurAbstrait.java
@@ -1,9 +1,11 @@
 import java.util.*;
-
+//Classe abstraite définissant la structure de base pour tout type d'analyseur
 public abstract class AnalyseurAbstrait {
 
+    // Méthode abstraite que les sous-classes
     public abstract List<Statistique> analyser(List<String[]> lignes);
 
+    // Méthode utilitaire pour vérifier si une chaîne de caractères peut être convertie en nombre
     protected boolean estNombre(String valeur) {
         try {
             Double.parseDouble(valeur);
