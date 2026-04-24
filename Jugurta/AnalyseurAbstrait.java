84487a974e6c4d84670116277aafb64cc94bf353
jugurta687 <haddadjugurta@outlook.fr>
2026-04-24 10:51:51 -0400

    Update AnalyseurAbstrait.java

--- Jugurta/AnalyseurAbstrait.java
+++ Jugurta/AnalyseurAbstrait.java
@@ -2,7 +2,7 @@ import java.util.*;
 //Classe abstraite définissant la structure de base pour tout type d'analyseur
 public abstract class AnalyseurAbstrait {
 
-    // Méthode abstraite que les sous-classes
+    // Méthode abstraite que les sous-classes herite
     public abstract List<Statistique> analyser(List<String[]> lignes);
 
     // Méthode utilitaire pour vérifier si une chaîne de caractères peut être convertie en nombre
